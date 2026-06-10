893ea48ba67cec976e927cbe012f1ac34836099d
Dharmik Patel <47842695+Dharmik1710@users.noreply.github.com>
2026-06-10 12:26:14 -0700

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.py
@@ -0,0 +1,49 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
+        if not node:
+            return None
+
+        def dfsClone(node: Optional['Node']) -> None:
+            if node in visited:
+                return
+
+            visited.add(node)
+            for n in node.neighbors:
+                dfsClone(n)
+            node.neighbors.append(Node(node.val))
+        
+        def addNeighbors(node: Optional['Node']) -> None:
+            if node in visited:
+                return
+
+            visited.add(node)
+            node_copy = node.neighbors[-1]
+            for n in node.neighbors[:-1]:
+                node_copy.neighbors.append(n.neighbors[-1])
+                addNeighbors(n)
+        
+        def popCopy(node: Optional) -> Optional['Node']:
+            if node in visited:
+                return None
+
+            visited.add(node)
+            for n in node.neighbors[:-1]:
+                popCopy(n)
+            
+            return node.neighbors.pop() if node else None
+        
+        visited = set()
+        dfsClone(node)
+        visited.clear()
+        addNeighbors(node)
+        visited.clear()
+        return popCopy(node)
